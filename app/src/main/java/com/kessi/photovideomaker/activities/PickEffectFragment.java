d2d8db51ed1120ce2ef9c73f7ab024a9596d0f8a
Thanh Tuyên <matthaus079@gmail.com>
2022-04-27 12:05:24 +0700

    Tuyen commit: fragment

--- app/src/main/java/com/kessi/photovideomaker/activities/PickEffectFragment.java
+++ app/src/main/java/com/kessi/photovideomaker/activities/PickEffectFragment.java
@@ -1,4 +1,27 @@
 package com.kessi.photovideomaker.activities;
 
-public class PickEffectFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.kessi.photovideomaker.R;
+
+public class PickEffectFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.pick_effect_fragment, container, false);
+        SetUp(view);
+
+        return view;
+    }
+
+    private void SetUp(View view){
+
+    }
 }
